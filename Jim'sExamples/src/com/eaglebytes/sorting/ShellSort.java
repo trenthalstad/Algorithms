5a1f22121c34e184ca072e8a67817e4474eeaa71
Legendaries <legendwaitforitaries@gmail.com>
2015-10-29 16:33:24 -0600

    Update ShellSort.java

--- Jim'sExamples/src/com/eaglebytes/sorting/ShellSort.java
+++ Jim'sExamples/src/com/eaglebytes/sorting/ShellSort.java
@@ -4,12 +4,12 @@ package com.eaglebytes.sorting;
 public class ShellSort implements Sort{
 
     @Override
-    public void sort(){
+    public void sort(int[] data){
         
     }
     
     @Override
-    public void sortStep(){
+    public void sortStep(int[] data){
         
     }
 
