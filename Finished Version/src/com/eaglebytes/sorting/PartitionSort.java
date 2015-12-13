93f9f49241d11cfd2ab5d15e7d802d5226f0cab7
Legendaries <legendwaitforitaries@gmail.com>
2015-12-13 14:33:20 -0700

    Added a folder for the finished version

---
+++ Finished Version/src/com/eaglebytes/sorting/PartitionSort.java
@@ -0,0 +1,41 @@
+
+package com.eaglebytes.sorting;
+
+public class PartitionSort implements Sort {
+    
+    public PartitionSort(){
+        
+    }
+    
+    public int temp = 0;
+    
+    @Override
+    public void init(int[] data){
+        
+    }
+
+    @Override
+    public int[] sort(int[] data){
+        return data;
+    }
+    
+    @Override
+    public int[] sortStep(int[] data){
+        return data;
+    }
+    
+    @Override
+    public int getTemp(){
+        return temp;
+    }
+    
+    @Override
+    public int getTempIndex(){
+        return 0;
+    }
+    
+    @Override
+    public int getTempIndex2(){
+        return 0;
+    }
+}
